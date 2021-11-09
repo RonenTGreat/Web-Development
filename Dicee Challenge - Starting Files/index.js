fe5bbc5dc0b0bc557fbeb836e0c5aa18796fca12
ronentgreat <ronenh53@gmail.com>
2021-11-09 14:33:27 +0000

    Day 15

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,13 +1,19 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".dice .img1").setAttribute("src", "images//dice" + randomNumber1 + ".png");
+document.querySelector(".dice .img1").setAttribute("src", "images//dice" + randomNumber1 + ".png"); // Changes the img1
+
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".dice .img2").setAttribute("src", "images//dice" + randomNumber2 + ".png");
+document.querySelector(".dice .img2").setAttribute("src", "images//dice" + randomNumber2 + ".png"); // Changes the img2 
 
+// If player 1 wins
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").textContent = "🚩Player 1 Wins";
-}else if(randomNumber1 < randomNumber2){
+}
+// IF player 2 wins
+else if(randomNumber1 < randomNumber2){
     document.querySelector("h1").textContent = "Player 2 Wins🚩";
-}else{
+}
+// Draw
+else{
     document.querySelector("h1").textContent = "Draw!";
 }
