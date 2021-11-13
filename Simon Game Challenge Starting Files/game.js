acb6158d1f91d5c9743a3aa5b4bf660286d75b3b
ronentgreat <ronenh53@gmail.com>
2021-11-13 10:10:25 +0000

    Day 19: Added flash and sound to the Simon project

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -5,6 +5,10 @@ function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 3);
     randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    console.log(gamePattern);
-}
+    
 
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var sound = new Audio("sounds\\" + randomChosenColour + ".mp3");
+    sound.play();
+}
