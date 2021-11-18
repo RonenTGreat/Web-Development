3b9a85b05325acc374b7b9231b9623e9f415960d
ronentgreat <ronenh53@gmail.com>
2021-11-18 12:00:59 +0000

    completed: step 8

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -16,49 +16,51 @@ $(document).keypress(function () {
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+  }
+}
+
 function nextSequence() {
-    level++;
+  userClickedPattern = [];
+  level++;
 
-    $("#level-title").text("Level " + level);
+  $("#level-title").text("Level " + level);
 
-  var randomNumber = Math.floor(Math.random() * 3);
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour)
-    .fadeOut(100)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-    playSound(randomChosenColour)
+  playSound(randomChosenColour);
 }
 
 function playSound(name) {
-  var randomNumber = Math.floor(Math.random() * 3);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-
   var sound = new Audio("sounds\\" + name + ".mp3");
   sound.play();
-
-  $(".btn").click(function () {
-    var sound = new Audio("sounds\\" + $(this).attr("id") + ".mp3");
-    sound.play();
-  });
 }
 
 function animatePress(currentColour) {
-  $("#" + currentColour).click(function () {
-    $(this).addClass("pressed");
-
-    setTimeout(function () {
-      $("#" + currentColour).removeClass("pressed");
-    }, 100);
-  });
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
